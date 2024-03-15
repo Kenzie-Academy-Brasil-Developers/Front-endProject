126f230a55d6e8932fd71a9ef2f22169d92f010d
gabizinha <gabrielamiyazaki55@gmail.com>
2024-03-15 08:42:03 -0300

    pagina de registro estilizada

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,10 +1,15 @@
 import { RegisterForm } from "../../components/Forms/FormRegister"
-
+import styles from "./style.module.scss"
 export const RegisterPage = () => {
     return(
         <main className="container">
-            <div className="formulario, div">
-                <RegisterForm />
+            <div className={styles.divContent}>
+                <div>
+                </div>
+                <div className="formulario, div">
+                    <h1 className={styles.title}>Cadastre-se</h1>
+                    <RegisterForm />
+                </div>
             </div>
         </main>
     )
